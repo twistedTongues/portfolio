cdffbc02cd9b2b928de593eefc3f66cbe5aca0b7
Mark <flossiedixkeybounce@gmail.com>
2022-11-01 18:14:11 +0300

    added-about-and-workexperience-section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,39 @@
 import Head from "next/head";
+import { About } from "../components/About";
 import { Header } from "../components/Header";
+import { Hero } from "../components/Hero";
+import { Projects } from "../components/Projects";
+import { Skills } from "../components/Skills";
+import { WorkExperience } from "../components/WorkExperience";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>TwistedTongues Portfolio</title>
       </Head>
 
-      {/* Header */}
       <Header />
 
-      {/* Hero  */}
+      <section id="hero" className="snap-start">
+        <Hero />
+      </section>
 
-      {/* About  */}
+      <section id="about" className="snap-center">
+        <About />
+      </section>
 
-      {/* Experience */}
+      <section id="experience" className="snap-center">
+        <WorkExperience />
+      </section>
 
-      {/* Skills */}
+      <section id="skills" className="snap-center">
+        <Skills />
+      </section>
 
-      {/* Projects */}
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
 
       {/* Contact me */}
     </div>
