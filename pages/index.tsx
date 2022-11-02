f3f04527cf47f098471072a15758fc70e61e9330
Mark <flossiedixkeybounce@gmail.com>
2022-11-02 11:44:11 +0300

    added-projects-skill-contactme-section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { About } from "../components/About";
+import { ContactMe } from "../components/ContactMe";
 import { Header } from "../components/Header";
 import { Hero } from "../components/Hero";
 import { Projects } from "../components/Projects";
@@ -8,7 +9,7 @@ import { WorkExperience } from "../components/WorkExperience";
 
 export default function Home() {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0">
       <Head>
         <title>TwistedTongues Portfolio</title>
       </Head>
@@ -27,7 +28,7 @@ export default function Home() {
         <WorkExperience />
       </section>
 
-      <section id="skills" className="snap-center">
+      <section id="skills" className="snap-start">
         <Skills />
       </section>
 
@@ -35,7 +36,9 @@ export default function Home() {
         <Projects />
       </section>
 
-      {/* Contact me */}
+      <section id="contact" className="snap-start">
+        <ContactMe />
+      </section>
     </div>
   );
 }
